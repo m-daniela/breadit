878048dd1e56b2f2249a563abf469d7636d7828e
m-daniela <54980615+m-daniela@users.noreply.github.com>
2023-06-20 14:36:56 +0300

    1_breadit_App_Setup: Initialized app, added routes

---
+++ src/app/(user_pages)/dashboard/page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/**
+ * Dashboard page
+ * This will be different based on the role 
+ * (user, moderator, admin), but it will 
+ * contain information about the posts and
+ * comments the user has interacted with
+ * TODO: this will be functional when user
+ * TODO: support will be added
+ */
+const Dashboard = () => {
+    return (
+        <div>
+            dashboard, different based on role
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/app/boards/[board_name]/page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * Board page
+ * Displays the list of posts from the 
+ * board
+ * Each post will have a preview of the 
+ * contents
+ * TODO: add filtering, sorting
+ */
+const Board = () => {
+    return (
+        <div>
+            board page, posts will be displayed
+        </div>
+    );
+};
+
+export default Board;
